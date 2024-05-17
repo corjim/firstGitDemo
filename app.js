a72487a50c66a4a3fe8c389fa33ac9cf339fc570
MartinOrji <martin.orji.jnr@gmail.com>
2024-05-17 10:49:27 -0500

    add css, js to file

---
+++ app.js
@@ -0,0 +1,21 @@
+function randomRBG() {
+    const r = Math.floor(Math.random() * 256);
+    const g = Math.floor(Math.random() * 256);
+    const b = Math.floor(Math.random() * 256);
+    return `rgb(${r},${g},${b})`
+}
+
+// const h1 = document.querySelector("h1");
+// function colorfunc(){
+//   h1.style.color = randomRBG();  
+// }
+
+// setInterval(colodrfunc, 1000);
+
+const letters = document.getElementsByClassName('letter');
+const loopRGB = () => {
+    for (let letter of letters) {
+        letter.style.color = randomRBG();
+    }
+}
+setInterval(loopRGB, 500);
